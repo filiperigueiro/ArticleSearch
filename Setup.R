6d3eaa28e145e1b33405a16245f097e54f79773d
Filipe Rigueiro <filipe.rigueiro@stjohnslabs.com>
2018-11-15 17:43:00 +0000

    Start

--- Setup.R
+++ Setup.R
@@ -1,8 +1,9 @@
-install.packages("RISmed")
+
+#PUBMED
 library(RISmed)
 
 search_topic <- "abcam"
-search_query <- EUtilsSummary(search_topic, mindate = 2000, maxdate=2018, retmax = 10000)
+search_query <- EUtilsSummary(search_topic, mindate = 2000, maxdate=2018, retmax = 1000)
 
 records <- EUtilsGet(search_query)
 
@@ -14,3 +15,15 @@ DATA <- data.frame(Title = records@ArticleTitle,
 
 
 pubmed_data <- data.frame('Title'=ArticleTitle(records),'Abstract'=AbstractText(records))
+
+ls <- list(ID = records@PMID, Title = records@ArticleTitle, Author = records@Author)
+
+
+###SCOPUS
+library(rscopus)
+options("elsevier_api_key" = "dc1e6fa872979812a50e0e3ca4920700")
+library(dplyr)
+res = author_df(last_name = "Muschelli", first_name = "John", verbose = FALSE, general = FALSE)
+names(res)
+
+test <- scopus_search("abcam")
